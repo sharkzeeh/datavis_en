1167d726e9353b744ead0192bd88fbed9febd3f4
Igor Babikov <igorbabikov24@gmail.com>
2020-03-30 12:46:34 +0300

    add hw1: html + js

--- hw/hw1/js/index.js
+++ hw/hw1/js/index.js
@@ -1,2 +1,18 @@
 const g = document.querySelector('g');
-const circle = document.querySelector('circle');
+const path = document.querySelector('path');
+const checkBox = document.getElementById('smileCheck');
+const label = document.getElementById('smileLabel');
+
+function change_string_param(pos) {
+    path.setAttribute('d', `M 150 200 Q 225 ${pos} 300 200`)
+}
+
+function fun() {
+    if (checkBox.checked) {
+        change_string_param(300);
+        label.innerHTML="Happy";
+    } else {
+        change_string_param(150);
+        label.innerHTML="Sad";
+    }
+}
